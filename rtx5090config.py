f2cfb04772c6c47d2113195664f8610810c1edbf
Senaxx <michelbeld@gmail.com>
2025-02-19 16:45:48 +0100

    Create rtx5090config.py
    Configuratie bestand om code bases voor de 5090 en 5080 beter te onderhouden.

---
+++ rtx5090config.py
@@ -0,0 +1,46 @@
+# Proxy Configuration
+PROXY_IPS = [
+  'xxxxxx', 'xxxxxx', 'xxxxxx', 'xxxxxx',
+  'xxxxxx', 'xxxxxx1', 'xxxxxx', 'xxxxxx',
+  'xxxxxx', 'xxxxxx'
+]
+
+PROXY_USERNAME = "xxxxxx"
+PROXY_PASSWORD = "xxxxxx"
+
+# Product Configuration
+PRODUCT_NAME = "NVIDIA RTX 5090"
+BASE_URL = "https://api.store.nvidia.com/partner/v1/feinventory?status=1&skus={sku}&locale={locale}"
+SEARCH_API_URL = "https://api.nvidia.partners/edge/product/search?page=1&limit=12&locale=fi-fi&category=GPU"
+
+#Telegram Message
+BASE_MESSAGE = "RTX 5090FE te koop in {country}!\n\nMarketplace: https://marketplace.nvidia.com/{locale}/consumer/graphics-cards/"
+
+# Locale Configuration
+LOCALES_COUNTRIES = {
+    "nl-nl": ("🇳🇱", "Nederland"),
+    "de-de": ("🇩🇪", "Duitsland"),
+    "da-dk": ("🇩🇰", "Denemarken"),
+    "da-at": ("🇦🇹", "Oostenrijk"),
+    "fi-fi": ("🇫🇮", "Finland")
+}
+
+# Headers Configuration
+API_HEADERS = {
+    "authority": "api.nvidia.partners",
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,nl;q=0.7",
+    "content-type": "application/json",
+    "dnt": "1",
+    "origin": "https://marketplace.nvidia.com",
+    "priority": "u=1, i",
+    "referer": "https://marketplace.nvidia.com/",
+    "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Microsoft Edge";v="132"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0"
+}
